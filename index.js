df48f2b4bb0859fa99668bf497ceb222b93f87f6
NithinRegidi <regidinithinsainaidu@gmail.com>
2025-09-07 09:49:08 +0530

    Created separate init.js for mongodb connections

--- index.js
+++ index.js
@@ -4,19 +4,9 @@ const app = express();
 const port = 5000;
 const path = require('path');
 
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-
-
-// Mongoose
-const mongoose = require('mongoose');
-main()
-.then(() => { console.log(" Connected to MongoDB ") })
-.catch(err => console.log(err));
-async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/chitchat');
-}
-
 
 app.get("/", (req, res) => {
     res.send("Working");
---
+++ init.js
@@ -0,0 +1,50 @@
+
+// Mongoose
+const mongoose = require('mongoose');
+main()
+.then(() => { console.log(" Connected to MongoDB ") })
+.catch(err => console.log(err));
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/chitchat');
+}
+
+const chat = require("./models/chat");
+
+// Array of chat data
+let allChats = [
+    {
+        from: "neha",
+        to: "priya",
+        msg: "send me update",
+        created_at: new Date()
+    },
+    {
+        from: "ram",
+        to: "raj",
+        msg: "When we go outside?",
+        created_at: new Date()
+    },
+    {
+        from: "john",
+        to: "doe",
+        msg: "Let's meet tomorrow.",
+        created_at: new Date()
+    },
+    {
+        from: "alice",
+        to: "bob",
+        msg: "Can you review my code?",
+        created_at: new Date()
+    }
+];
+
+chat.insertMany(allChats)
+    .then((res) => {
+        console.log("chats Saved");
+        console.log(res);
+    })
+    .catch((err) => {
+        console.log("Error in saving chats");
+        console.error(err);
+    })
+
