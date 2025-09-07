ee6f7fb47a7fdafb1acd000ffc3a4b58926cd18f
NithinRegidi <regidinithinsainaidu@gmail.com>
2025-09-07 11:52:47 +0530

    Till now I add Edit button too, now it is CRU D operated basic app, I need to add Delete

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const port = 5000;
 const path = require('path');
 const chat = require('./models/chat');
 
+const methodOverride = require("method-override");
+app.use(methodOverride("_method"));
+
 // Middleware to parse form data
 app.use(express.urlencoded({ extended: true })); 
 
@@ -21,6 +24,9 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.get("/main", (req, res) => {
+    res.render("main.ejs");
+})
+app.get("/", (req, res) => {
     res.render("main.ejs");
 })
 
@@ -49,7 +55,7 @@ app.post("/newchat", async (req, res) => {
         if (!from || !to || !msg) {
             return res.status(400).send("All fields are required");
         }
-        
+
         const newChat = new chat({from, to, msg, created_at : new Date()});
         await newChat.save();
         res.redirect("chats");
@@ -61,12 +67,40 @@ app.post("/newchat", async (req, res) => {
 
 
 
+// Edit the chat
+app.get("/chats/:id/edit", async (req, res) => {
+    try {
+        let { id } = req.params;
+        let chatData = await chat.findById(id);
+
+        if (!chatData) {
+            return res.status(404).send("Chat not found");
+        }
+
+        res.render("edit.ejs", { chat: chatData });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Error loading chat for editing");
+    }
+});
 
 
-
-
-
-
+// Update the chat
+app.put("/chats/:id", async (req, res) =>{
+    try {
+        let {id} = req.params;
+        let {newMsg} = req.body;
+        let updatedChat = await chat.findByIdAndUpdate(
+            id,
+            {msg : newMsg},
+            {runValidators : true, new : true}
+        )
+        res.redirect("/chats");
+    } catch (error) {
+       console.error(error);
+        res.status(500).send("Error updating chat");
+    }
+})
 
 // App is running
 app.listen(port, () => {
